804e60056a84b1e32a874fa7b5973061b89bb6d3
artcabrera <jbcabrera.910@gmail.com>
2023-06-16 04:44:37 +0800

    ADD: TailwindCSS dep

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+import stylesheet from "~/tailwind.css"
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -11,7 +11,7 @@ import {
 import { Analytics } from "@vercel/analytics/react";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export default function App() {
