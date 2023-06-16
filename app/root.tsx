2f19c0830c25be03d3b76a0ddeffb9c258e41adb
artcabrera <jbcabrera.910@gmail.com>
2023-06-16 08:03:47 +0800

    ERR: Early Dev

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import stylesheet from "~/tailwind.css"
-import type { LinksFunction } from "@remix-run/node";
+import stylesheet from '~/tailwind.css';
+import type { LinksFunction, V2_MetaFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -7,29 +7,56 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import { Analytics } from "@vercel/analytics/react";
+  useRouteError
+} from '@remix-run/react';
+import type { PropsWithChildren } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import Layout from './root.layout';
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
+const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
 
-export default function App() {
+const meta: V2_MetaFunction = () => [];
+
+const Document = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <meta
+          name='description'
+          content='Devoted to constructing refined softwares with precision and crafting remarkable web experiences.'
+        />
+        <link rel='canonical' href='https://www.jorndoescode.lol' />
         <Meta />
         <Links />
       </head>
       <body>
+        {children}
         <Analytics />
-        <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+};
+
+const CatchBoundary = () => {
+  const error = useRouteError();
+  console.error('Err', error);
+};
+
+export { links, meta, CatchBoundary };
+
+export default App;
